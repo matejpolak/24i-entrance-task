b063ef6e4836d25a942800a1c1d3d8bb5eaef98a
Matěj Polák <matpolak@seznam.cz>
2017-11-13 16:50:57 +0100

    MyGoogle is displaying right results, not designed yet...

---
+++ build/static/js/ImageResult.js
@@ -0,0 +1,44 @@
+/*
+    Class for proper pages results
+
+    Functionality
+    1) Looping through search results
+    2) Use correct piece of information in right place
+    3) Append final <div> into main file
+ */
+
+function Image(obj) {
+
+    this.result = obj;
+    this.title = obj.title;
+    this.displayLink = obj.displayLink;
+    this.link = obj.link;
+    this.width = obj.image.width;
+    this.height = obj.image.height;
+    this.thumbnail = obj.image.thumbnailLink;
+
+
+    this.printImage();
+};
+
+Image.prototype = {
+
+    result: null,
+    title: null,
+    displayLink: null,
+    link: null,
+    width: null,
+    height: null,
+    thumbnail: null,
+
+printImage: function () {
+    var finalResult =
+        '<div class="image-result">' +
+            '<div class="thumbnail"><img src="' + this.thumbnail + '" alt="' + this.title + '"></div>' +
+            '<div class="size-link">' + this.width + 'x' + this.height + ' - ' + this.displayLink + '</div>' +
+        '</div>';
+
+    $('.images').prepend(finalResult);
+}
+}
+;
---
+++ build/static/js/PageResult.js
@@ -0,0 +1,41 @@
+/*
+    Class for proper pages results
+
+    Functionality
+    1) Looping through search results
+    2) Use correct piece of information in right place
+    3) Append final <div> into main file
+ */
+
+function Page(obj) {
+
+    this.result = obj;
+    this.displayLink = obj.displayLink;
+    this.title = obj.htmlTitle;
+    this.snippet = obj.htmlSnippet;
+    this.url = obj.link;
+
+
+    this.printPage();
+};
+
+Page.prototype = {
+
+    result: null,
+    displayLink: null,
+    title: null,
+    snippet: null,
+    url: null,
+    image: null,
+
+    printPage: function() {
+        var finalResult =
+            '<div class="page-result">' +
+                '<div class="title"><a href="' + this.url + '">' + this.title + '</a></div>' +
+                '<div class="link"><a href="' + this.url + '">' + this.url + '</a></div>' +
+                '<div class="snippet"><p>' + this.snippet + '</p></div>' +
+            '</div>';
+
+        $('.pages').append(finalResult);
+    }
+};
